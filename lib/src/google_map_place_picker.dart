a0a6f9e80237a5f6ea0b29e00bea882f34e5ae64
Ypsolution <ypsolution12@gmail.com>
2022-09-13 14:16:55 +0530

    Fix the issue

--- lib/src/google_map_place_picker.dart
+++ lib/src/google_map_place_picker.dart
@@ -335,15 +335,18 @@ class GoogleMapPlacePicker extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 10),
-          RaisedButton(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          ElevatedButton(
+          //  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             child: Text(
               "Select here",
               style: TextStyle(fontSize: 16),
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4.0),
-            ),
+            ),style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  )
+              )
+          ),
             onPressed: () {
               onPlacePicked!(result);
             },
